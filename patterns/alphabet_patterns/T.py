9053b3c090bcfe29d6ca76e38cbc922cf2111c6d
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-06-12 21:01:14 +0530

    committed changes

---
+++ patterns/alphabet_patterns/T.py
@@ -0,0 +1,11 @@
+n = 5
+
+for i in range(n):
+    for j in range(n):
+
+        if i == 0 or j == n//2:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+
+    print()
