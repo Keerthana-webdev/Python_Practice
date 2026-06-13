ef43a69bfdd4c8f0fe3b1a30262140307ad8e591
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-06-13 21:33:35 +0530

    committed changes

---
+++ patterns/alphabet_patterns/U.py
@@ -0,0 +1,12 @@
+n = 5
+
+for i in range(n):
+    for j in range(n):
+
+        if ((j == 0 or j == n-1) and i != n-1) or \
+           (i == n-1 and j != 0 and j != n-1):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+
+    print()
