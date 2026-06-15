8ea00cba28e4b7eb961544b2aa067bd2c27a7e60
keerthweb-1519 <yashodhakeerthi19@gmail.com>
2026-06-15 19:19:06 +0530

    committed changes

---
+++ patterns/alphabet_patterns/W.py
@@ -0,0 +1,13 @@
+n = 5
+
+for i in range(n):
+    for j in range(2 * n - 1):
+
+        if j == 0 or j == 2 * n - 2 or \
+           (i + j == n - 1 and i >= n//2) or \
+           (j - i == n - 1 and i >= n//2):
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+    print()
